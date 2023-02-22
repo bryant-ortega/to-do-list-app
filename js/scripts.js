f2a25d1ee5c26a4fdeed28c767a3f936b49f5dbf
Bryant Ortega <kalicorose@gmail.com>
2023-02-21 23:19:15 -0800

    linked css in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,41 @@
-function newItem() {
-    let li = $("<li></li>");
-    let inputValue = $("#input").val();
-    li.append(inputValue);
 
-    if (inputValue === "") {
-        alert("you must write something!!");
-    } else {
-        $("#list").append(li);
-    }
 
-    li.on("dblclick", function crossOut() {
-        li.toggleClass("strike");
-    });
 
-    let crossOutButton = $("<crossOutButton></crossOutButton>");
-    crossOutButton.append(document.createTextNode("X"));
-    li.append(crossOutButton);
 
-    crossOutButton.on("click", deleteListItem);
-    function deleteListItem() {
-        li.addClass("delete");
-    }
 
-    $("#list").sortable();
-}
+
+
+
+
+
+
+
+
+
+
+// function newItem() {
+//     let li = $("<li></li>");
+//     let inputValue = $("#input").val();
+//     li.append(inputValue);
+
+//     if (inputValue === "") {
+//         alert("you must write something!!");
+//     } else {
+//         $("#list").append(li);
+//     }
+
+//     li.on("dblclick", function crossOut() {
+//         li.toggleClass("strike");
+//     });
+
+//     let crossOutButton = $("<crossOutButton></crossOutButton>");
+//     crossOutButton.append(document.createTextNode("X"));
+//     li.append(crossOutButton);
+
+//     crossOutButton.on("click", deleteListItem);
+//     function deleteListItem() {
+//         li.addClass("delete");
+//     }
+
+//     $("#list").sortable();
+// }
