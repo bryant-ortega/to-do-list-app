18e307d7e29f995b4b09101bd37e52965f30e2ab
Bryant Ortega <kalicorose@gmail.com>
2023-02-21 23:12:48 -0800

    replaced base code in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,23 @@
 function newItem() {
     let li = $("<li></li>");
-    let inputValue = $("input").val();
+    let inputValue = $("#input").val();
     li.append(inputValue);
 
     if (inputValue === "") {
-        alert("You must write something!");
+        alert("you must write something!!");
     } else {
         $("#list").append(li);
     }
 
-    function crossOut() {
+    li.on("dblclick", function crossOut() {
         li.toggleClass("strike");
-    }
-
-    li.on("dblclick", crossOut);
+    });
 
     let crossOutButton = $("<crossOutButton></crossOutButton>");
     crossOutButton.append(document.createTextNode("X"));
     li.append(crossOutButton);
 
     crossOutButton.on("click", deleteListItem);
-
     function deleteListItem() {
         li.addClass("delete");
     }
